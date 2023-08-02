bb02dfef48a1736aefac5e33d780a4580447956e
Federico Rossi <federico.rossi@phd.unipi.it>
2023-08-02 09:56:30 +0200

    Added backend for PTA and hwbaseback trait

---
+++ include/backends/hwbaseback.hpp
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C) 2017-2023 Emanuele Ruffaldi, Federico Rossi
+ * 
+ * Distributed under the terms of the BSD 3-Clause License.  
+ * 
+ * (See accompanying file LICENSE)
+ * 
+ * --
+ */
+
+
+namespace posit {
+
+    struct HwBaseBackend {};
+    
+}
